7bdf8c75b28e058b92145e40954159dbd64e05f2
Tca1107 <tom.ch.auer@googlemail.com>
2015-08-23 20:53:01 +0200

    Current User implementiert
    User bleibt jetzt die ganze Zeit eingeloggt. Dementsprechend startet die
    App, sollte sich der User nicht ausgeloggt haben, im Home Screen und
    nicht im Login Screen.

--- Regensbad/app/src/main/java/com/example/tom/regensbad/HomeScreenActivity.java
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/HomeScreenActivity.java
@@ -1,18 +1,42 @@
 package com.example.tom.regensbad;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 
 public class HomeScreenActivity extends ActionBarActivity {
+
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        // debugging and testing
+        testButton = (Button)findViewById(R.id.button_test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+
+            }
+        });
+
+
     }
+
+
+
 
 
     @Override
