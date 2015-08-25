faebe90d78191a852eacc430ef6dadee3cfb4d3b
Tca1107 <tom.ch.auer@googlemail.com>
2015-08-25 18:52:45 +0200

    Bisschen Layout und Suchfunktion angefangen

--- Regensbad/app/src/main/java/com/example/tom/regensbad/Activities/HomeScreenActivity.java
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/Activities/HomeScreenActivity.java
@@ -1,13 +1,19 @@
 package com.example.tom.regensbad.Activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tom.regensbad.R;
 import com.parse.ParseUser;
@@ -15,21 +21,62 @@ import com.parse.ParseUser;
 
 public class HomeScreenActivity extends ActionBarActivity {
 
-// Hier ein Comment, um zu sehen, dass git läuft
+    /* Constant of the type String that defines the filepath of the "Pacifico" font used for the main heading. */
+    private static final String FONT_PACIFICO_FILE_PATH = "Fonts/Pacifico.ttf";
+
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        //initializeUIElements();
+        initializeActionBar();
+       // setFontOfAppName();
 
     }
+
+
+    /* This method was written using the tutorial "How to customize / change ActionBar font, text, color, icon, layout and so on
+    with Android", which is available at:
+     http://www.javacodegeeks.com/2014/08/how-to-customize-change-actionbar-font-text-color-icon-layout-and-so-on-with-android.html .*/
+    private void initializeActionBar() {
+        this.getSupportActionBar().setDisplayShowCustomEnabled(true);
+        this.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.home_screen_action_bar, null);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), FONT_PACIFICO_FILE_PATH);
+        ((TextView)view.findViewById(R.id.text_view_action_bar_home_screen)).setTypeface(typeface);
+        this.getSupportActionBar().setCustomView(view);
+    }
+
+
+    /*
+    private void initializeUIElements() {
+        appName = (TextView)findViewById(R.id.text_view_app_name_in_home_screen_activity);
+    }
+
+    */
+
+
+
+
+
+    /* This method was created using the tutorial on including external fonts in Android Studio which can be found
+    * at the following website: http://www.thedevline.com/2014/03/how-to-include-fonts-in-android.html .
+    * The font used is a font of Google Fonts named "Pacifico", which can be found at the following website:
+    * https://www.google.com/fonts/ .
+    private void setFontOfAppName() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), FONT_PACIFICO_FILE_PATH);
+        appName.setTypeface(typeface);
+    } */
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
