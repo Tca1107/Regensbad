0706bdfb437a4495cfd9a5cb14e8a874c7e54cbf
Peacecake <peacecake@web.de>
2015-08-26 13:13:19 +0200

    CivicPool hinzugefügt

---
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/Domain/CivicPool.java
@@ -0,0 +1,69 @@
+package com.example.tom.regensbad.Domain;
+
+
+import java.net.URI;
+
+/**
+ * Created by Sebastian on 26.08.2015.
+ */
+public class CivicPool {
+
+    private String name;
+    private String type;
+    private double lati;
+    private double longi;
+    private Number phoneNumber;
+    private URI website;
+    private double openTime;
+    private double closeTime;
+    private String picPath;
+
+    public CivicPool (String name ,String type, double lati, double longi, Number phoneNumber, URI website, double openTime, double closeTime, String picPath){
+        this.name = name;
+        this.type = type;
+        this.lati = lati;
+        this.longi = longi;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.picPath = picPath;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public double getLati(){
+        return lati;
+    }
+
+    public double getLongi(){
+        return longi;
+    }
+
+    public Number getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public URI getWebsite(){
+        return website;
+    }
+
+    public double getOpenTime(){
+        return openTime;
+    }
+
+    public double getCloseTime(){
+        return closeTime;
+    }
+
+    public String getPicPath(){
+        return picPath;
+    }
+
+}
