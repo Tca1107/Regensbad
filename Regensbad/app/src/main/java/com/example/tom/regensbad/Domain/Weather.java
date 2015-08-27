58954479df2cbba9d939c66af443b8de8a33e922
Tca1107 <tom.ch.auer@googlemail.com>
2015-08-27 14:52:03 +0200

    updated weather object and weatherJSONConverter

--- Regensbad/app/src/main/java/com/example/tom/regensbad/Domain/Weather.java
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/Domain/Weather.java
@@ -6,17 +6,56 @@ package com.example.tom.regensbad.Domain;
 public class Weather {
 
     private String degrees;
+    private String maxDegrees;
+    private String minDegrees;
+    private String humidity;
+    private long sunrise;
+    private long sunset;
+    private String windSpeed;
     private String weatherDescription;
     private String weatherIcon;
 
-    public Weather (String degrees, String weatherDescription, String weatherIcon) {
+
+
+    public Weather (String degrees, String maxDegrees, String minDegrees, String humidity, long sunrise, long sunset,
+                    String windSpeed, String weatherDescription, String weatherIcon) {
         this.degrees = degrees;
+        this.maxDegrees = maxDegrees;
+        this.minDegrees = minDegrees;
+        this.humidity = humidity;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+        this.windSpeed = windSpeed;
         this.weatherDescription = weatherDescription;
         this.weatherIcon = weatherIcon;
     }
 
     public String getDegrees () {
         return degrees;
+    }
+
+    public String getMaxDegrees () {
+        return maxDegrees;
+    }
+
+    public String getMinDegrees () {
+        return minDegrees;
+    }
+
+    public String getHumidity () {
+        return humidity;
+    }
+
+    public long getSunrise () {
+        return sunrise;
+    }
+
+    public long getSunset () {
+        return sunset;
+    }
+
+    public String getWindSpeed () {
+        return windSpeed;
     }
 
     public String getWeatherDescription () {
@@ -26,6 +65,8 @@ public class Weather {
     public String getweatherIcon () {
         return weatherIcon;
     }
+
+
 
 
 
--- Regensbad/app/src/main/java/com/example/tom/regensbad/Persistence/WeatherJSONConverter.java
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/Persistence/WeatherJSONConverter.java
@@ -18,6 +18,14 @@ public class WeatherJSONConverter {
     private static final String WEATHER = "weather";
     private static final String DESCRIPTION = "description";
     private static final String ICON_CODE = "id";
+    private static final String SYS = "sys";
+    private static final String SUNRISE = "sunrise";
+    private static final String SUNSET = "sunset";
+    private static final String MAX_DEGREES = "temp_max";
+    private static final String MIN_DEGREES = "temp_min";
+    private static final String HUMIDITY = "humidity";
+    private static final String WIND = "wind";
+    private static final String WIND_SPEED = "speed";
 
     private String JSONResult;
     private Weather weatherToReturn;
@@ -32,14 +40,32 @@ public class WeatherJSONConverter {
             JSONObject jsonObject = new JSONObject(JSONResult);
             JSONObject main = jsonObject.getJSONObject(MAIN);
             String degrees = main.getString(DEGREES);
+            String maxDegrees = main.getString(MAX_DEGREES);
+            String minDegrees = main.getString(MIN_DEGREES);
+            String humidity = main.getString(HUMIDITY);
+            JSONObject sys = jsonObject.getJSONObject(SYS);
+            long sunrise = sys.getLong(SUNRISE);
+            long sunset = sys.getLong(SUNSET);
+            JSONObject wind = jsonObject.getJSONObject(WIND);
+            String windSpeed = wind.getString(WIND_SPEED);
             JSONArray weather = jsonObject.getJSONArray(WEATHER);
             JSONObject weatherObject = weather.getJSONObject(0);
             String weatherDescription = weatherObject.getString(DESCRIPTION);
             String weatherIcon = weatherObject.getString(ICON_CODE);
+
+            //Logs for debugging
+            Log.d("windgeschwind", windSpeed);
+            Log.d("humidity", humidity);
+            Log.d("minDegrees", minDegrees);
+            Log.d("maxDegrees", maxDegrees);
+            Log.d("sunrise", String.valueOf(sunrise));
+            Log.d("sunset", String.valueOf(sunset));
             Log.d("degrees:", degrees);
             Log.d("weatherDescription", weatherDescription);
             Log.d("weatherIcon", weatherIcon);
-            weatherToReturn = new Weather (degrees, weatherDescription, weatherIcon);
+
+            weatherToReturn = new Weather (degrees, maxDegrees, minDegrees, humidity, sunrise, sunset,
+                    windSpeed, weatherDescription, weatherIcon);
         } catch (JSONException exception) {
             exception.printStackTrace();
         }
