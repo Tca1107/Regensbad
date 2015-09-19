1042db12c8d455b7d91fb689811e19804c3318b4
Tca1107 <tom.ch.auer@googlemail.com>
2015-09-19 14:13:21 +0200

    paar änderungen

---
+++ Regensbad/app/src/main/java/com/example/tom/regensbad/HelperClasses/SearchHelper.java
@@ -0,0 +1,18 @@
+package com.example.tom.regensbad.HelperClasses;
+
+/**
+ * Created by Tom on 19.09.2015.
+ */
+public class SearchHelper {
+
+    private String searchQuery;
+
+    public SearchHelper (String searchQuery) {
+        this.searchQuery = searchQuery;
+    }
+
+
+
+
+
+}
